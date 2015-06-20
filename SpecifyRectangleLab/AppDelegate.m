89756cd851b911cd62cdfeeed17453a2b0cbb006
TokiM <tokim6@gmail.com>
2015-06-20 18:44:03 +0900

    Add custom view to xib.

--- SpecifyRectangleLab/AppDelegate.m
+++ SpecifyRectangleLab/AppDelegate.m
@@ -9,18 +9,19 @@
 #import "AppDelegate.h"
 
 @interface AppDelegate ()
-
-@property (weak) IBOutlet NSWindow *window;
+@property (nonatomic, weak) IBOutlet NSWindow * window;
 @end
 
 @implementation AppDelegate
 
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    // Insert code here to initialize your application
+- (void)applicationDidFinishLaunching:(NSNotification *)notification
+{
+    
 }
 
-- (void)applicationWillTerminate:(NSNotification *)aNotification {
-    // Insert code here to tear down your application
+- (void)applicationWillTerminate:(NSNotification *)notification
+{
+    
 }
 
 @end
